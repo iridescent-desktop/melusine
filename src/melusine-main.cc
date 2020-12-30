09154028bebe064d9d77c80bbf1f6f632ad5607e
Ariadne Conill <ariadne@dereferenced.org>
2020-12-30 12:07:23 -0700

    add program skeleton

---
+++ src/melusine-main.cc
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2020 Ariadne Conill <ariadne@dereferenced.org>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * This software is provided 'as is' and without any warranty, express or
+ * implied.  In no event shall the authors be liable for any damages arising
+ * from the use of this software.
+ */
+
+#include <QtWaylandShellHelpers/qwaylandshellapplication.h>
+#include <QtWaylandShellHelpers/qxdgtoplevelwindow.h>
+#include <QtWaylandShellHelpers/qlayershellwindow.h>
+#include <QWidget>
+#include <QLabel>
+#include <QVBoxLayout>
+#include <QPushButton>
+
+int
+main(int argc, char **argv)
+{
+	QtWaylandShellHelpers::QWaylandShellApplication app(argc, argv);
+
+	return app.exec();
+}
